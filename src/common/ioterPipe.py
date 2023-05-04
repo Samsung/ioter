4122616923ac2554ba34fbc70a30a2f61fc9785b
constant.lee <constant.lee@samsung.com>
2023-05-04 13:57:51 +0900

    remove unnecessary code

--- src/common/ioterPipe.py
+++ src/common/ioterPipe.py
@@ -10,30 +10,22 @@ class PipeThread(QThread):
     setup_code = pyqtSignal(str)
     received_qrcode = pyqtSignal(str)
 
-    # 초기화 메서드 구현
     def __init__(self, deviceNum):
         super().__init__()
         self.deviceNum = deviceNum
 
     def run(self):
-        # 쓰레드로 동작시킬 함수 내용 구현
         FIFO_FILENAME = '/tmp/chip_pipe_device' + self.deviceNum
         if not os.path.exists(FIFO_FILENAME):
             os.mkfifo(FIFO_FILENAME)
-        if os.path.exists(FIFO_FILENAME):
-            fp_fifo = open(FIFO_FILENAME, "r")
+        fp_fifo = open(FIFO_FILENAME, "r")
 
         while True:
-            with open(FIFO_FILENAME, 'r') as fifo:
-                data = fifo.read()
-                line = data.split('\n')
-                # print(line)
-
-            for msg in line:
-                if msg != '':
-                    now = datetime.now()
-                    print("[%s][%s] %s" % (str(now), self.deviceNum, msg))
-                    self.msg_changed.emit(msg)  # send to main thread
+            data = fp_fifo.readline().rstrip('\n')
+            if data:
+                now = datetime.now()
+                print("[%s][%s] %s" % (str(now), self.deviceNum, data))
+                self.msg_changed.emit(data)  # send to main thread
 
     def stop(self):
         self.quit()
