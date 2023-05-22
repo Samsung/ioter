25698bd148adcb4dfc676c5265e45e53e58627c2
Manjunath N Sataraddi <m.sataraddi@samsung.com>
2023-05-22 18:08:48 +0530

    Added comments for src/auto_onboarding/ and src/winman.
    Signed-off-by: Manjunath N Sataraddi <m.sataraddi@samsung.com>

--- src/auto_onboarding/auto_devicelayout.py
+++ src/auto_onboarding/auto_devicelayout.py
@@ -1,13 +1,49 @@
+###########################################################################
+#
+#BSD 3-Clause License
+#
+#Copyright (c) 2023, Samsung Electronics Co.
+#All rights reserved.
+#
+#Redistribution and use in source and binary forms, with or without
+#modification, are permitted provided that the following conditions are met:
+#1. Redistributions of source code must retain the above copyright
+#   notice, this list of conditions and the following disclaimer.
+#2. Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+#3. Neither the name of the copyright holder nor the
+#   names of its contributors may be used to endorse or promote products
+#   derived from this software without specific prior written permission.
+#
+#THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+#LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+#CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+#SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+#INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+#CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+#ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#POSSIBILITY OF SUCH DAMAGE.
+#
+###########################################################################
+# File : auto_devicelayout.py
+# Description:
+# Handles UI Setup of automation
+
 from common.utils import Utils
 from common.device_command import *
 
 from PyQt5 import QtCore, QtWidgets
 import os
 
-
+## Automation of devices ##
 class auto_device(object):
     index = 0
 
+    ## UI setup ##
     def setupUi(self, parent, comport, vendor):
         self.parent = parent
         self.comport = comport
@@ -60,10 +96,12 @@ class auto_device(object):
         self.insert_comboBox()
         QtCore.QMetaObject.connectSlotsByName(parent)
 
+    ## Insert Combo Box ##
     def insert_comboBox(self):
         self.combo_thread_type.addItems(self.get_thread_type_list())
         self.combo_device_type.addItems(CommandUtil.device_type_id.keys())
 
+    ## Get thread type ##
     def get_thread_type_list(self):
         version = []
         list = os.listdir(Utils.get_thread_lib_path())
@@ -73,5 +111,6 @@ class auto_device(object):
             version.append(file[len(Utils.get_thread_lib_prefix()):])
         return version
 
+    ## Set status ##
     def set_status(self):
         self.status.setText()
