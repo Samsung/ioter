cc5019578050e0d6101d434ab747d285a05eb5dd
vedansh <v.bhardwaj@samsung.com>
2023-05-23 17:10:06 +0530

    Updated comment for src/automation
    Signed-off-by: vedansh <v.bhardwaj@samsung.com>

--- src/automation/devicelayout.py
+++ src/automation/devicelayout.py
@@ -1,3 +1,38 @@
+###########################################################################
+#
+#BSD 3-Clause License
+#
+#Copyright (c) 2023, Samsung Electronics Co.
+#All rights reserved.
+#
+#Redistribution and use in source and binary forms, with or without
+#modification, are permitted provided that the following conditions are met:
+#1. Redistributions of source code must retain the above copyright
+#   notice, this list of conditions and the following disclaimer.
+#2. Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+#3. Neither the name of the copyright holder nor the
+#   names of its contributors may be used to endorse or promote products
+#   derived from this software without specific prior written permission.
+#
+#THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+#LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+#CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+#SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+#INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+#CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+#ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#POSSIBILITY OF SUCH DAMAGE.
+#
+###########################################################################
+# File : devicelayout.py
+# Description:
+# Setup Device command Layout.
+
 from common.device_command import CommandUtil
 
 from PyQt5 import QtCore, QtGui, QtWidgets
@@ -6,8 +41,8 @@ import xml.etree.ElementTree as Etree
 
 class Ui_Device(object):
     index = 0
-########## Setup Device command Layout ##########
 
+    ## Setup Device command Layout ##
     def setupUi(self, parent):
         self.objectName = 'cmd'
         self.layoutWidget = QtWidgets.QWidget(parent.scrollAreaWidgetContents)
@@ -57,6 +92,7 @@ class Ui_Device(object):
         self.datainsert(parent)
         QtCore.QMetaObject.connectSlotsByName(parent)
 
+    ## Retranslate UI ##
     def retranslateUi(self, parent):
         _translate = QtCore.QCoreApplication.translate
         self.btn_remove.setText(_translate("parent", "-"))
@@ -64,13 +100,15 @@ class Ui_Device(object):
         self.btn_up.setText(_translate("parent", "↑"))
         self.btn_down.setText(_translate("parent", "↓"))
 
+    ## Insert Data ##
     def datainsert(self, parent):
         for device in parent.devMgrObj.get_used_devices():
             if device.get_commissioning_state():
                 num = device.get_device_num()
                 self.comboBox_devicetype.addItem(
-                    CommandUtil.get_device_type_by_device_id(device.get_device_id())+'-'+num)
+                    CommandUtil. get_device_type_by_device_id(device. get_device_id())+'-'+num)
 
+    ## Show Device Type And respective Commands ##
     def changecomboBox_devicetype(self, parent):
         self.comboBox_cmd.clear()
         devicetype = self.comboBox_devicetype.currentText()
@@ -87,7 +125,7 @@ class Ui_Device(object):
                     data.append(str(x))
                 self.comboBox_cmd.addItem(cmd['Name'], data)
         self.comboBox_cmd.show()
-
+    ## Change Combo Box Command ##
     def changecomboBox_cmd(self, index):
         self.comboBox_value.clear()
         if index == -1:
@@ -101,6 +139,7 @@ class Ui_Device(object):
             self.comboBox_value.setEditable(False)
         self.comboBox_value.show()
 
+    ## Change Combo Box Value ##
     def changecomboBox_value(self, index):
         devtype = self.comboBox_devicetype.currentText()
         if 'Temperature' in devtype:
@@ -109,6 +148,7 @@ class Ui_Device(object):
             except:
                 self.comboBox_value.clear()
 
+    ## Removing Objects ##
     def deleteSelf(self, parent):
         print('removing obj...')
         self.layoutWidget.setParent(None)
@@ -117,9 +157,11 @@ class Ui_Device(object):
         parent.objs.remove(self)
         parent.adjustGeometry()
 
+    ## Calling Insert Dialog Function ##
     def addnew(self, parent):
         parent.insertDialog(self.index)
 
+    ## Move commands Up and Down ##
     def move_up_down(self, move, parent):
         y_axis = self.layoutWidget.pos().y()
         if move == "UP":
--- src/automation/logtrackscriptcntl.py
+++ src/automation/logtrackscriptcntl.py
@@ -1,3 +1,38 @@
+###########################################################################
+#
+#BSD 3-Clause License
+#
+#Copyright (c) 2023, Samsung Electronics Co.
+#All rights reserved.
+#
+#Redistribution and use in source and binary forms, with or without
+#modification, are permitted provided that the following conditions are met:
+#1. Redistributions of source code must retain the above copyright
+#   notice, this list of conditions and the following disclaimer.
+#2. Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+#3. Neither the name of the copyright holder nor the
+#   names of its contributors may be used to endorse or promote products
+#   derived from this software without specific prior written permission.
+#
+#THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+#LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+#CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+#SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+#INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+#CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+#ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#POSSIBILITY OF SUCH DAMAGE.
+#
+###########################################################################
+# File : logtrackscriptcntl.py
+# Description:
+# Handle Automation Script Logs.
+
 from common.utils import Utils
 
 import psutil
@@ -13,14 +48,17 @@ class LogTrackScriptCntl():
         self.filename = None
         self.successlogfile = Utils.get_tmp_path() + 'success.log'
 
+    ## Get Product ID ##
     def get_pid(self):
         # print("current pid : " + str(self.pid))
         return self.pid
 
+    ## Set Product ID ##
     def set_pid(self, pid):
         print("PID change from " + str(self.pid) + " to " + str(pid))
         self.pid = pid
 
+    ## Launch Log track Script ##
     def launch_log_track_script(self):
         if os.path.isfile(self.successlogfile):
             os.remove(self.successlogfile)
@@ -29,17 +67,20 @@ class LogTrackScriptCntl():
         self.set_pid(self.subProcess.pid)
         return self.pid
 
+    ## Terminate Log Track Script ##
     def terminate_log_track_script(self, terminated=True):
         # time.sleep(2)
         self.terminate_process_tree()
         if (terminated):
             print('Log taken')
 
+    ## Check Success File ##
     def checksuccessfile(self):
         if self.pid != -1 and os.path.isfile(self.successlogfile):
             return True
         return False
 
+    ## Terminate Process Tree ##
     def terminate_process_tree(self):
         if self.pid == -1:
             print(
@@ -61,6 +102,7 @@ class LogTrackScriptCntl():
         self.subProcess.poll()
         self.set_pid(-1)
 
+    ## Find Exd Acknowledgement ##
     def findRxdAck(self, data, msg_counter):
         index = -1
         try:
@@ -70,6 +112,7 @@ class LogTrackScriptCntl():
             print('Rxd Ack is not found for', msg_counter)
         return index
 
+    ## Return Success Count ##
     def successcount(self, filename):
         if not os.path.isfile(self.successlogfile):
             print(self.successlogfile, 'does not exisit!')
